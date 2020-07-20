a9d5f43d047eaa8ae0b46518fead307df580ffef
Alejandro Cataño Herrera <alejandrocatanoherrera@gmail.com>
2020-07-20 16:47:25 -0500

    make the users migration

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\RecetaController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,6 +20,7 @@ Route::get('/', function () {
 });
 
 Route::get('/recetas', 'RecetaController');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
