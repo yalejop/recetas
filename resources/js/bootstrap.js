d4ec46c89c00437f4f0dd2d5dde0408f8046da0d
Alejandro Cataño Herrera <alejandrocatanoherrera@gmail.com>
2020-07-23 09:30:15 -0500

    add momentJs Library to project

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -39,3 +39,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
+
+//Instalar MomentJS
+
+window.moment = require('moment');
